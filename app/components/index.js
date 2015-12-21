5aad57a2b78c1ab08146d760c109a58f5211e229
unutranyholas <unutranyholas@gmail.com>
2015-12-21 12:16:44 +0300

    Fix bugs

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,6 @@
 export { default as App }  from './App/App';
 export { default as DataPoint } from  './DataPoint/DataPoint';
 export { default as OptionsMenu } from './OptionsMenu/OptionsMenu';
-export { default as ConfigProp } from  './ConfigProp/ConfigProp';
 export { default as CodeEditor } from  './CodeEditor/CodeEditor';
 export { default as Chart } from  './Chart/Chart';
 export { default as DataManager } from  './DataManager/DataManager';
